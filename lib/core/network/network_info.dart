c9a00ceb1a0cae3c8c761cc502aad2c4b2156c88
Shravan <shravan@elred.io>
2025-05-22 18:24:57 +0530

    network_info added to check internet connection

---
+++ lib/core/network/network_info.dart
@@ -0,0 +1,18 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  const NetworkInfoImpl(this.connectivity);
+
+  final Connectivity connectivity;
+
+  @override
+  Future<bool> get isConnected async {
+    return await connectivity.checkConnectivity().then(
+      (result) => !(result.contains(ConnectivityResult.none)),
+    );
+  }
+}
