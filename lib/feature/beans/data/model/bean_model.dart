1c2abb9091f5a8fea165ddc58ed52b7b3fc7756c
Shravan <shravan@elred.io>
2025-05-22 16:47:48 +0530

    feat: BeanModel

--- lib/feature/beans/data/model/bean_model.dart
+++ lib/feature/beans/data/model/bean_model.dart
@@ -17,10 +17,36 @@ class BeanModel extends Bean {
   });
 
   factory BeanModel.fromMap(Map<String, dynamic> map) {
-    throw UnimplementedError();
+    return BeanModel(
+      id: map["beanId"] as int,
+      groupName: List<String>.from(map["groupName"] as List),
+      ingredients: List<String>.from(map["ingredients"] as List),
+      flavorName: map["flavorName"] as String,
+      description: map["description"] as String,
+      colorGroup: map["colorGroup"] as String,
+      backgroundColor: map["backgroundColor"] as String,
+      imageUrl: map["imageUrl"] as String,
+      glutenFree: map["glutenFree"] as bool,
+      sugarFree: map["sugarFree"] as bool,
+      seasonal: map["seasonal"] as bool,
+      kosher: map["kosher"] as bool,
+    );
   }
 
   Map<String, dynamic> toMap() {
-    throw UnimplementedError();
+    return {
+      "beanId": id,
+      "groupName": groupName,
+      "ingredients": ingredients,
+      "flavorName": flavorName,
+      "description": description,
+      "colorGroup": colorGroup,
+      "backgroundColor": backgroundColor,
+      "imageUrl": imageUrl,
+      "glutenFree": glutenFree,
+      "sugarFree": sugarFree,
+      "seasonal": seasonal,
+      "kosher": kosher,
+    };
   }
 }
