9ceb7406c7bf36166c17a594511d5dbafde010ae
Shravan <shravan@elred.io>
2025-06-05 10:07:56 +0530

    test di added

--- test/feature/beans/display/view/step/the_app_is_running.dart
+++ test/feature/beans/display/view/step/the_app_is_running.dart
@@ -1,9 +1,9 @@
 import 'package:bdd_jelly_belly/application.dart';
-import 'package:bdd_jelly_belly/injection_container.dart' as di;
+import 'package:bdd_jelly_belly/injection_container.dart' as test_di;
 import 'package:flutter_test/flutter_test.dart';
 
 Future<void> theAppIsRunning(WidgetTester tester) async {
-  await di.init();
+  await test_di.init();
   await tester.pumpWidget(Application());
   await tester.pumpAndSettle();
 }
---
+++ test/feature/beans/display/view/test_injection_container.dart
@@ -0,0 +1,42 @@
+import 'package:bdd_jelly_belly/core/network/network_info.dart';
+import 'package:bdd_jelly_belly/feature/beans/data/data_source/bean_local_data_source.dart';
+import 'package:bdd_jelly_belly/feature/beans/data/data_source/bean_remote_data_source.dart';
+import 'package:bdd_jelly_belly/feature/beans/data/repository/bean_repository_impl.dart';
+import 'package:bdd_jelly_belly/feature/beans/display/view_model/bean_view_model.dart';
+import 'package:bdd_jelly_belly/feature/beans/domain/repository/bean_repository.dart';
+import 'package:bdd_jelly_belly/feature/beans/domain/usecase/get_beans_use_case.dart';
+import 'package:bdd_jelly_belly/injection_container.dart';
+
+import '../../data/repository/bean_repository_impl_test.mocks.dart';
+
+Future<void> init() async {
+  await sl.reset();
+
+  // view model
+  sl.registerFactory<BeanViewModel>(() => BeanViewModel(getBeansUseCase: sl()));
+
+  // use cases
+  sl.registerLazySingleton<GetBeansUseCase>(
+    () => GetBeansUseCase(repository: sl()),
+  );
+
+  // repository
+  sl.registerLazySingleton<BeanRepository>(
+    () => BeanRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  // data sources
+  sl.registerLazySingleton<BeanRemoteDataSource>(
+    () => MockBeanRemoteDataSource(),
+  );
+  sl.registerLazySingleton<BeanLocalDataSource>(
+    () => MockBeanLocalDataSource(),
+  );
+
+  // core
+  sl.registerLazySingleton<NetworkInfo>(() => MockNetworkInfo());
+}
